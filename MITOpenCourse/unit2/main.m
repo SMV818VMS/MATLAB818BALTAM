e5cd919ad5b2cbacc03cb3c4dcb307466f56f3bf
Samuel Miravet Verde <samuelmiver@gmail.com>
2015-10-20 17:02:43 +0200

    Unit2 Lesson Functions and plotting finished // Starting assignment

--- MITOpenCourse/unit2/main.m
+++ MITOpenCourse/unit2/main.m
@@ -1,4 +1,4 @@
-% Unit2 script
+% Unit2 script: VISUALIZATION AND PROGRAMMING
 % script including the trials and ideas presented in the lecture 2
 
 %%%
@@ -18,3 +18,117 @@ plotSin(2);
 %%%
 % FLOW CONTROL
 %%%
+
+% Exercise: conditional
+% Modification of the function plotSin
+% plotSin(2,4); % Two inputs were given
+% plotSin(); plotSin(1,2,4); % Incorrect number of arguments, I only accept 1
+
+%%%
+% LINE PLOTS
+%%%
+x = linspace(0,10,10);
+figure
+plot(x,x, 'k.-'); % Plots in black dots + line
+plot(x,x, '.'); % plots only dots
+
+% Line and marker options
+figure
+x=-pi:pi/100:pi;
+y=cos(4*x).*sin(10*x).*exp(-abs(x));
+plot(x, y ,'--s','LineWidth',2,...
+    'Color', [1 0 0], ...
+    'MarkerEdgeColor','k',...
+    'MarkerFaceColor','g',...
+    'MarkerSize',10)
+
+% Cartesian Plots
+figure
+plot(x,y,'k-');
+
+% The same is applied for semilog and loglog plots
+figure
+semilogx(x, y, 'k');
+semilogy(y, 'r.-');
+loglog(x, y);
+
+figure; x=0:100; semilogy(x, exp(x), 'k.-');
+
+% 3D line plots
+figure;
+time=0:0.001:4*pi;
+x=sin(time);
+y=cos(time);
+z=time;
+plot3(x,y,z,'k','LineWidth',2);
+zlabel('Time');
+close all
+
+% Slide 23 --> axis modes
+% Slide 24 --> multiple plots in one Figure
+
+% Exercise: ADVANCED PLOTTING
+plotSin(6)
+% plotSin(1,2)
+close all % close all figure
+
+%%%
+% VISUALIZING MATRICES
+%%%
+figure;
+mat = reshape(1:10000,100,100);
+imagesc(mat);
+colorbar;
+caxis([3000 7000]);
+close all
+
+% slide 31 to define special color maps
+
+% SURFACE PLOTS
+x = -pi:0.1:pi;
+y = -pi:0.1:pi;
+[X,Y] = meshgrid(x, y);
+Z = sin(X).*cos(Y);
+surf(x,y,Z);
+shading interp; % Slide 34 surf options
+close all
+
+% CONTOUR
+figure(1)
+contour(X,Y,Z,'LineWidth',2)
+hold on
+mesh(X,Y,Z)
+saveas(1, './figures/contourplussurf.png', 'png');
+close all
+
+% EXERCISE: Modifying plotSin
+plotSin(3,4);
+close all
+
+% SPECIAL PLOTTING FUNCTIONS
+polar(0:0.01:2*pi,cos((0:0.01:2*pi)*2))
+
+bar(1:10,rand(1,10));
+
+[X,Y]=meshgrid(1:10,1:10);
+quiver(X,Y,rand(10),rand(10));
+
+stairs(1:10,rand(1,10));
+
+fill([0 1 0.5],[0 0 1],'r');
+
+clear all
+close all
+
+%%%
+% VECTORIZATION
+%%%
+
+% Revisiting find
+x=rand(1,100);
+inds = find(x>0.4 & x<0.6);
+
+% Avoiding loops --> count values >0 
+count=length(find(x>0));
+
+% ART OF AVOIDING LOOPS --> VECTORIZATION --> MORE EFFICIENT :D
--- MITOpenCourse/unit2/plotSin.m
+++ MITOpenCourse/unit2/plotSin.m
@@ -1,9 +1,26 @@
 % plotSin(f1) function
 % given f1, plots the function sin(f1x)
 
-function plotSin(f1)
-x = linspace(0, 2*pi, f1*16+1);
-h = figure;
-set(h, 'visible','off');
-plot(x, sin(f1*x), 'b');
-saveas(h,'./figures/lect2slide9.png','png');
+function plotSin(f1, f2)
+    x = linspace(0, 2*pi, f1*16+1);
+if nargin == 1
+    h = figure;
+    set(h, 'visible','off');
+    plot(x, sin(f1*x) , '--s','LineWidth',2,...
+    'Color', [1 0 0], ...
+    'MarkerEdgeColor','r',...
+    'MarkerFaceColor','k',...
+    'MarkerSize',5)
+    saveas(h,'./figures/lect2slide9.png','png');
+elseif nargin == 2
+    y=linspace(0,2*pi,round(16*f2)+1);
+    [X,Y]=meshgrid(x,y);
+    Z=sin(f1*X)+sin(f2*Y);
+    subplot(2,1,1); imagesc(x,y,Z); colorbar;
+    axis xy; colormap hot
+    subplot(2,1,2); surf(X,Y,Z);
+else
+    disp('Incorrect number of arguments, I only accept 1')
+end
+
+    
